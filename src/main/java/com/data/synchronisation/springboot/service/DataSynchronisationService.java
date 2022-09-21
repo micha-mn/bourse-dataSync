a521b8fb3e2dbd031dd71ea6bbddff7fe4c62582
jeanSaade-SpBt <57522990+micha-mn@users.noreply.github.com>
2022-09-21 16:29:19 +0300

    latest

--- src/main/java/com/data/synchronisation/springboot/service/DataSynchronisationService.java
+++ src/main/java/com/data/synchronisation/springboot/service/DataSynchronisationService.java
@@ -16,7 +16,24 @@ public class DataSynchronisationService {
 	 @PersistenceContext
 	 private EntityManager entityManager;
 	 
+	 /*
 	 @Async
+	 public void triggerRobot(String robotName,String columnDescription) throws InterruptedException {
+	     System.out.println("excuting Robot for column: "+ robotName +" "+ columnDescription+" time:"+new Date());
+	 	try {
+	     StoredProcedureQuery query = this.entityManager.createStoredProcedureQuery(robotName);
+	     query.registerStoredProcedureParameter("columnDescription", String.class, ParameterMode.IN);
+		 query.setParameter("columnDescription",columnDescription );
+		 query.execute();
+	     // Artificial delay of 1s for demonstration purposes
+	     // Thread.sleep(1000L);
+	 	} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	 }
+	 */
+	 
 	 public void triggerRobot(String robotName,String columnDescription) throws InterruptedException {
 	     System.out.println("excuting Robot for column: "+ robotName +" "+ columnDescription+" time:"+new Date());
 	 	try {
